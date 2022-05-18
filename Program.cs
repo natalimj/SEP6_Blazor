2dd2dd3ef0ac16602259f6ed7fa9723d26a71486
natalimj <natalimj88@gmail.com>
2022-05-18 10:16:32 +0200

    changed Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IMovieService, MovieService>();
-builder.Services.AddScoped<ITVService, TVService>();
+builder.Services.AddScoped<IProductionService, ProductionService>();
 builder.Services.AddScoped<IActorService, ActorService>();
 
 builder.Services.AddOidcAuthentication(options =>
