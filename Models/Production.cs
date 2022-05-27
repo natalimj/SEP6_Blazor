47ede1e72489c34de5c65b642e4659806a3dc34a
popaDaria <popadaria19@gmail.com>
2022-05-27 16:13:34 +0200

    TC-28 added avg actor rating

--- Models/Production.cs
+++ Models/Production.cs
@@ -6,23 +6,15 @@ namespace SEP6_Blazor.Models
     public class Production
     {
         [JsonPropertyName("id")] public int Id { get; set; }
-
-        [JsonPropertyName("original_language")]
-        public string OriginalLanguage { get; set; }
-
+        [JsonPropertyName("original_language")] public string OriginalLanguage { get; set; } = "";
         [JsonPropertyName("original_title")] public string MovieName { get; set; }
-
         [JsonPropertyName("overview")] public string Overview { get; set; }
-
         [JsonPropertyName("poster_path")] public string Poster_path { get; set; }
-
         [JsonPropertyName("vote_average")] public double VoteAverage { get; set; }
-        
         [JsonPropertyName("vote_count")] public int VoteCount { get; set; }
-
-        [JsonPropertyName("genres")]
-        public List<Genre> MovieGenres { get; set; } //only for movies! - don't use for tvshow
-
+        [JsonPropertyName("genres")] public List<Genre> MovieGenres { get; set; } //only for movies! - don't use for tvshow
         [JsonPropertyName("original_name")] public string TVShowName { get; set; }
+        [JsonPropertyName("title")] public string EnglishTitle { get; set; } = "";
+        [JsonPropertyName("name")] public string EnglishName { get; set; } = "";
     }
 }
