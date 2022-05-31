01dac39e9945c13ce05689c5bfeb03852b8a641e
Bogdan Mezei <bogdan335@hotmail.com>
2022-06-01 00:40:40 +0200

    TC-20 ProductionService Unit Tests

--- BlazorUnitTests/UnitTest1.cs
+++ BlazorUnitTests/UnitTest1.cs
@@ -1,6 +1,6 @@
-
-using BootstrapBlazor.Components;
+using System.Collections.Generic;
 using SEP6_Blazor.Data;
+using SEP6_Blazor.Models;
 using Xunit;
 using Console = System.Console;
 
@@ -24,16 +24,116 @@ public class UnitTest1
     [Fact]
     public void Test2()
     {
-        var discoveryService = new DiscoveryService();
-        var discoveryResult = discoveryService.DiscoveryProduction("", "", "2017", "", "", "", "", "", "").Result.ProductionResults[0].MovieName;
-        Console.Write(discoveryResult);
         Assert.True(true);
+    }
+    
+    // ProductionService
+    [Fact]
+    public void SearchProduction()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Uncharted", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetProduction(resultSearch, "movie").Result.Id.ToString();
+        Assert.Equal(resultGet,resultSearch);
     }
     [Fact]
-    public void Test3()
+    public void GetProduction()
     {
-        Assert.True(true);
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetProduction(resultSearch, "movie").Result.Id.ToString();
+        Assert.Equal(resultGet,resultSearch);
     }
+    
+    [Fact]
+    public void GetProductionImagesUrl()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetProductionImagesUrl(resultSearch, "movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Images>(resultGet);
+    }
+    
+    [Fact]
+    public void GetCast()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetCast(resultSearch, "movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Cast>(resultGet);
+    }
+    [Fact]
+    public void GetDirector()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetDirector(resultSearch, "movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<List<Director>>(resultGet);
+    }
+    [Fact]
+    public void GetRecommendedProductions()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetRecommendedProductions(resultSearch, "movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Results>(resultGet);
+    }
+    
+    [Fact]
+    public void GetSimilarProductions()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetSimilarProductions(resultSearch, "movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Results>(resultGet);
+    }
+    [Fact]
+    public void GetPopularProductions()
+    {
+        var productionService = new ProductionService();
+        var resultGet = productionService.GetPopularProductions("movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Results>(resultGet);
+    }
+    [Fact]
+    public void GetLatestProduction()
+    {
+        var productionService = new ProductionService();
+        var resultGet = productionService.GetLatestProduction("movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Production>(resultGet);
+    }
+    [Fact]
+    public void GetTopRatedProduction()
+    {
+        var productionService = new ProductionService();
+        var resultGet = productionService.GetTopRatedProduction("movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Results>(resultGet);
+    }
+    [Fact]
+    public void GetUpcomingMovies()
+    {
+        var productionService = new ProductionService();
+        var resultGet = productionService.GetUpcomingMovies().Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<Results>(resultGet);
+    }
+    [Fact]
+    public void GetProductionReviews()
+    {
+        var productionService = new ProductionService();
+        var resultSearch = productionService.SearchProduction("Harry Potter", "movie").Result.ProductionResults[0].Id.ToString();
+        var resultGet = productionService.GetProductionReviews(resultSearch,"movie").Result;
+        Assert.NotNull(resultGet);
+        Assert.IsType<TMDBReview>(resultGet);
+    }
+    // UserService
     [Fact]
     public void Test4()
     {
