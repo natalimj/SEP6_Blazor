06cb58560806baebbe04d14ce3e64602f6b8e233
Bogdan Mezei <bogdan335@hotmail.com>
2022-05-31 16:18:33 +0200

    TC-20 Unit test

--- BlazorUnitTests/UnitTest1.cs
+++ BlazorUnitTests/UnitTest1.cs
@@ -1,18 +1,32 @@
 
+using BootstrapBlazor.Components;
+using SEP6_Blazor.Data;
 using Xunit;
+using Console = System.Console;
 
 namespace BlazorUnitTests;
 
 public class UnitTest1
 {
+    // ActorService
     [Fact]
-    public void Test1()
+    public void SearchAndGetPerson()
     {
-        Assert.True(true);
+        var actorService = new ActorService();
+        var searchResult = actorService.SearchPerson("Johnny Depp").Result[0].Id;
+        var getPersonResult = actorService.GetPerson(searchResult.ToString()).Result.Id;
+        Console.Write(searchResult);
+        Console.Write(getPersonResult);
+        Assert.Equal(searchResult, getPersonResult);
+        
     }
+    // DiscoveryService
     [Fact]
     public void Test2()
     {
+        var discoveryService = new DiscoveryService();
+        var discoveryResult = discoveryService.DiscoveryProduction("", "", "2017", "", "", "", "", "", "").Result.ProductionResults[0].MovieName;
+        Console.Write(discoveryResult);
         Assert.True(true);
     }
     [Fact]
